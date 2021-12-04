514af0fa0dc2b68cb77b519093cbfbb802c0336f
David Zamora <dz9217@hotmail.com>
2021-12-04 05:14:41 -0500

    finish 4th activity

---
+++ Paso4Unad/src/paso4unad/Main.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package paso4unad;
+
+import com.sun.j3d.utils.geometry.ColorCube;
+import com.sun.j3d.utils.universe.SimpleUniverse;
+import javax.media.j3d.BranchGroup;
+
+/**
+ *
+ * @author David
+ */
+public class Main {
+
+    public Main() {
+     
+        
+        
+    }
+
+
+
+    public static void main(String[] args) {
+
+        
+        
+        
+        Xbox.main(null);
+        
+    }
+    
+}
